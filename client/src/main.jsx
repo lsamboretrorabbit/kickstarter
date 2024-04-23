1f4df06f213a537439a1c7769ffa50a0666c57f6
lsamboretrorabbit <lsambo@retrorabbit.co.za>
2024-04-23 15:50:18 +0200

    updated desired chainId

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+  <ThirdwebProvider desiredChainId={11155111}> 
     <Router>
       <StateContextProvider>
         <App />
